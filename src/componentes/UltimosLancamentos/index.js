6fd0598e08d67e573b88ba69ca9ccc9babeea688
camarada_nikc <nicole.p.goncalves@aluno.senai.br>
2026-04-29 11:30:08 -0300

    react

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,5 +1,8 @@
 import { livros } from "./dadosUltimosLancamentos"
 import styled from "styled-components";
+import { Titulo } from "../Titulo";
+import CardRecomenda from "../CardRecomenda";
+import imagemLivro from '../../imagens/livro2.png';
 
 const UltimosLancamentosContainer = styled.section`
    background-color: #EBECEE
@@ -8,15 +11,7 @@ const UltimosLancamentosContainer = styled.section`
    flex-direction: column;
 `
 
-const Titulo = styled.h2`
-   width: 100%;
-   padding: 30px 0;
-   background-color: #FFF;
-   color: #EB9b00;
-   font-size: 36px;
-   text-align: center;
-   margin:0; 
-`
+
 
 const NovosLivrosContainer = styled.div`
     margin-top: 30px;
@@ -29,13 +24,24 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos(){
     return(
         <UltimosLancamentosContainer>
-        <Titulo>ÚLTIMOS LANÇAMENTOS</Titulo>
+        <Titulo 
+        cor={"#000"}
+        tamanhoFonte={"36px"}
+
+        >ÚLTIMOS LANÇAMENTOS</Titulo>
         <NovosLivrosContainer>
         { livros.map( livro => (
             <img src={livro.src}/>
         ))}
         </NovosLivrosContainer>
+        <CardRecomenda
+             titulo="Talvez você se interesse por..."
+             subtitulo="Angular 11"
+             descricao="Construindo uma aplicação com a plataforma Google."
+             img={imagemLivro}
+        />
         </UltimosLancamentosContainer>
+        // props é uma personalização, tipo um css emline
     )
 }
 
